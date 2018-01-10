c07b5429b630e05576d427f00605d928febd72c4
Ognjen Gatalo <ognjeng95@gmail.com>
2018-01-10 20:52:17 +0100

    Deploy v1.2.1

--- bin/config.js
+++ bin/config.js
@@ -1,6 +1,6 @@
 // Configuration for app (routes to all of the templates, etc...)
 module.exports = {
-  cli_version: '1.2.0',
+  cli_version: '1.2.1',
   terminal_colors: {
     green: '\x1b[32m',
     red: '\x1b[31m',
